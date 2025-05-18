2288d5f83c19140b1ee131383e1cbb185bc5136c
sayansadman <sayansadman.24csedu.004@gmail.com>
2025-05-18 23:06:22 +0600

    update: home page

--- src/main/java/com/project/miniJobPortal/controller/NavigationController.java
+++ src/main/java/com/project/miniJobPortal/controller/NavigationController.java
@@ -1,6 +1,7 @@
 package com.project.miniJobPortal.controller;
 
 import com.project.miniJobPortal.entity.Job;
+import com.project.miniJobPortal.service.CandidateService;
 import com.project.miniJobPortal.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,14 +17,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class NavigationController {
 
     private final JobService jobService;
+    private final CandidateService candidateService;
 
     @Autowired
-    public NavigationController(JobService jobService) {
+    public NavigationController(JobService jobService, CandidateService candidateService) {
         this.jobService = jobService;
+        this.candidateService = candidateService;
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+
+
+        int jobCount = jobService.getJobCount();
+        int candidateCount = candidateService.getCandidateCount();
+
+        model.addAttribute("jobCount", jobCount);
+        model.addAttribute("candidateCount", candidateCount);
+
         return "home";
     }
 
@@ -31,7 +42,7 @@ public class NavigationController {
     public String showPostedJobs(
             Model model,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "1") int size) {
+            @RequestParam(defaultValue = "4") int size) {
 
         Pageable pageable = PageRequest.of(page, size);
         Page<Job> jobs = jobService.getPaginatedJobs(pageable);
@@ -42,11 +53,6 @@ public class NavigationController {
         model.addAttribute("pageSize", size);
 
         return "jobs-posted";
-    }
-
-    @GetMapping("/available-candidates")
-    public String availableCandidates() {
-        return "available-candidates";
     }
 
     @GetMapping("/register")
